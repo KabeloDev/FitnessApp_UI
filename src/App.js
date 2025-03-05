2a7d22cdb6ab05a08737a3c1546b29cfc290255a
Kabelo Makhanya <makhanyak2002@gmail.com>
2025-03-05 23:32:55 +0200

    Resolved merge conflict in App.css and App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
 import Home from './components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-
-
-function App() {
-  return <>
-  
-    <Home></Home>
-  
-  </>
-=======
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Home /> {/* Use the Home component */}
+    </>
   );
->>>>>>> 8caf3299efdd304f8770f20b00e48eb19cdd00a9
 }
 
 export default App;
